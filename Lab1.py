907da0987aba990ace09d281e70afbd7fa720428
Papaefstathiou Spyridon <126704976+Blue-Cultist@users.noreply.github.com>
2023-03-01 21:34:08 +0200

    Completed tasks 1-4

--- Lab1.py
+++ Lab1.py
@@ -3,5 +3,14 @@ import requests
 url = input("Enter URL\n")
 
 with requests.get(url) as response:
-    headers = response.headers
-    print(headers)
+    headers = dict(response.headers)
+    cookies = response.cookies
+    for i in headers.keys():
+        print("{}: {}".format(i,headers[i]))
+    print("\nWeb server software: {}".format(headers['Server']))
+    if "Set-Cookie" in headers.keys():
+        print("\nThe site uses cookies.\n")
+        for cookie in response.cookies:
+            print("Cookie name: {}  Expiry: {}".format(cookie.name, cookie.expires))
+    else:
+        print("\nThe site doesn't use cookies.")
