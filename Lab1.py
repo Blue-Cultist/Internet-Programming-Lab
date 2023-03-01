13e0162dad759d528f30ad11caf53ed2391a5ac1
Papaefstathiou Spyridon <126704976+Blue-Cultist@users.noreply.github.com>
2023-03-01 19:48:35 +0200

    .header testing

--- Lab1.py
+++ Lab1.py
@@ -1,19 +1,7 @@
 import requests
 
-
-def more(text):
-    count = 0
-    for line in text.split('\n'):
-        print(line)
-        count += 1
-        if count % 30 == 0:
-            reply = input('Show more (y/n)? ')
-            if reply == 'n':
-                break
-
-
 url = 'http://python.org/'
 
 with requests.get(url) as response:
-    html = response.text
-    more(html)
+    headers = response.headers
+    print(headers)
